17b903c1f4626c1a11c32f105182000b1e52e641
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-03-28 10:25:14 +0800

    feat: add download_file function for downloading files with retry logic

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,8 @@
+import time
 from pathlib import Path
+from typing import Optional
 
+import httpx
 import orjson
 
 from log import logger
@@ -30,3 +33,30 @@ def write_json(file_path: Path, data):
         logger.error(f"Error writing JSON file: {e}")
     except Exception as e:
         logger.error(f"Error writing JSON file: {e}")
+
+
+def download_file(
+    url: str,
+    file_path: Path,
+) -> Optional[Path]:
+    if file_path.exists() and file_path.stat().st_size > 100:
+        logger.info(f"File already exists: {file_path}")
+        return file_path
+
+    retry = 3
+
+    while retry > 0:
+        try:
+            with httpx.stream("GET", url) as response:
+                with open(file_path, "wb") as f:
+                    for chunk in response.iter_bytes():
+                        f.write(chunk)
+            return file_path
+        except Exception as e:
+            file_path.unlink(missing_ok=True)
+            logger.error(f"Error downloading from Atlas {retry} retries left.\t{e}.")
+            time.sleep(1)
+
+            retry -= 1
+
+    return None
