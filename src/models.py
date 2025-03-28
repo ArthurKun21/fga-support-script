a566ba751a270542117b27e8fa459463b4e43c2b
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-03-28 09:54:29 +0800

    feat: add name sanitization methods for SupportFolder and CraftEssenceData models

--- src/models.py
+++ src/models.py
@@ -1,5 +1,10 @@
+import re
+import unicodedata
 from dataclasses import dataclass, field
 from pathlib import Path
+
+# Sanitize the 'name' to ensure it's a valid Windows directory name
+INVALID_CHARS_PATTERN = r'[<>:"/\\|?*\x00-\x1f]|\.$'
 
 
 @dataclass
@@ -14,6 +19,35 @@ class SupportFolder:
     path: Path
     name: str
     idx: int
+
+
+def _preprocess_name(input_str) -> str:
+    """Removes accents from a string using unicode normalization.
+
+    Args:
+        input_str: The string to process.
+
+    Returns:
+        The string with accents removed.
+    """
+    nfkd_form = unicodedata.normalize("NFKD", input_str)
+    only_ascii = "".join([c for c in nfkd_form if not unicodedata.combining(c)])
+    return only_ascii
+
+
+def _cleanup_name(name: str) -> str:
+    """Cleans up the name by removing invalid characters and normalizing it.
+
+    Args:
+        name: The name to clean up.
+
+    Returns:
+        str: The cleaned-up name.
+    """
+    sanitized_name = re.sub(INVALID_CHARS_PATTERN, " ", name)
+    sanitized_name = sanitized_name.strip()
+    sanitized_name = _preprocess_name(sanitized_name)
+    return sanitized_name
 
 
 @dataclass
@@ -34,6 +68,16 @@ class ServantData:
         """
         return len(self.faces) == 0
 
+    @property
+    def sanitized_name(self):
+        """
+        Get the sanitized name of the servant.
+
+        Returns:
+            str: The sanitized name of the servant.
+        """
+        return _cleanup_name(self.name)
+
 
 @dataclass
 class CraftEssenceData:
@@ -50,3 +94,13 @@ class CraftEssenceData:
             bool: True if the craft essence data is empty, False otherwise.
         """
         return self.faces is None
+
+    @property
+    def sanitized_name(self):
+        """
+        Get the sanitized name of the craft essence.
+
+        Returns:
+            str: The sanitized name of the craft essence.
+        """
+        return _cleanup_name(self.name)
