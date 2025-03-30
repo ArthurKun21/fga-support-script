16e8acec2f361370ef3b7120917012b6e9622a7d
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-03-30 13:45:56 +0800

    chore: update CRAFT_ESSENCE_DATA_URL to new API endpoint

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,3 @@
 SERVANT_DATA_URL = "https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json"
 
-CRAFT_ESSENCE_DATA_URL = (
-    "https://api.atlasacademy.io/export/JP/basic_equip_lang_en.json"
-)
+CRAFT_ESSENCE_DATA_URL = "https://api.atlasacademy.io/export/JP/nice_equip_lang_en.json"
