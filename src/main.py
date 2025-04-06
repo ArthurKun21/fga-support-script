8877b9a7afd9bee75f589b9a696e86f9b39b9eef
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-04-06 12:54:57 +0800

    refactor: update main function to use async preprocessing for CraftEssenceData

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from loguru import logger
 
 import directory
 from log import setup_logger
+from models import CraftEssenceData
 from preprocess import process_craft_essence
 
 
@@ -15,9 +16,15 @@ async def main(debug: bool):
     if debug:
         logger.debug("Debug mode is enabled.")
 
+    ce_latest_data: list[CraftEssenceData] = []
+
+    async def preprocess_ce():
+        nonlocal ce_latest_data
+        ce_latest_data = await process_craft_essence()
+
     async with create_task_group() as tg:
         tg.start_soon(directory.build_index)
-        tg.start_soon(process_craft_essence)
+        tg.start_soon(preprocess_ce)
 
 
 @click.command()
