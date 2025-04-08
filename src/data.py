92bf8164927b12245e80c72c7c1726715e3c8b3b
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-04-08 09:26:07 +0800

    fix: enhance servant image processing and directory management

--- src/data.py
+++ src/data.py
@@ -2,10 +2,11 @@ from concurrent.futures import as_completed
 from pathlib import Path
 
 import cv2
-from anyio import open_file
+from anyio import open_file, to_thread
 from anyio.from_thread import start_blocking_portal
 from loguru import logger
 
+import image
 from enums import SupportKind
 from models import (
     Assets,
@@ -29,6 +30,21 @@ TEMP_SERVANT_DIR.mkdir(exist_ok=True, parents=True)
 
 TEMP_CE_DIR = TMP_DIR / CE
 TEMP_CE_DIR.mkdir(exist_ok=True, parents=True)
+
+OUTPUT_DIR = ROOT / "tmp"
+OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
+
+OUTPUT_SERVANT_DIR = OUTPUT_DIR / SERVANT
+OUTPUT_SERVANT_DIR.mkdir(exist_ok=True, parents=True)
+
+OUTPUT_SERVANT_COLOR_DIR = OUTPUT_DIR / f"{SERVANT}-color"
+OUTPUT_SERVANT_COLOR_DIR.mkdir(exist_ok=True, parents=True)
+
+OUTPUT_CE_DIR = OUTPUT_DIR / CE
+OUTPUT_CE_DIR.mkdir(exist_ok=True, parents=True)
+
+OUTPUT_CE_COLOR_DIR = OUTPUT_DIR / f"{CE}-color"
+OUTPUT_CE_COLOR_DIR.mkdir(exist_ok=True, parents=True)
 
 
 async def _download_and_confirm_asset(
@@ -105,7 +121,9 @@ async def process_servant_data(
         if debug and debug_index >= 5:
             break
 
-        temp_download_dir = TEMP_SERVANT_DIR / f"{latest_data.idx:04d}"
+        servant_directory_name = f"{latest_data.idx:04d}"
+
+        temp_download_dir = TEMP_SERVANT_DIR / servant_directory_name
         temp_download_dir.mkdir(exist_ok=True, parents=True)
 
         txt_name_file_path = temp_download_dir / f"{latest_data.sanitized_name}.txt"
@@ -142,7 +160,25 @@ async def process_servant_data(
             pass
 
         if new_assets_found:
-            pass
+            output_dir = OUTPUT_SERVANT_DIR / servant_directory_name
+            output_dir.mkdir(exist_ok=True, parents=True)
+
+            output_color_dir = OUTPUT_SERVANT_COLOR_DIR / servant_directory_name
+            output_color_dir.mkdir(exist_ok=True, parents=True)
+
+            await to_thread.run_sync(
+                image.create_support_servant_img,
+                temp_download_dir,
+                output_dir / "support.png",
+                output_color_dir / "support.png",
+            )
+
+            txt_file_path = f"{latest_data.sanitized_name}.txt"
+
+            (output_dir / txt_file_path).touch(exist_ok=True)
+            (output_color_dir / txt_file_path).touch(exist_ok=True)
+
+            logger.info(f"Servant images created for: {latest_data.sanitized_name}")
 
         if debug:
             debug_index += 1
