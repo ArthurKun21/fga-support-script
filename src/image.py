feaecc8caa8eceb55d4217809b10b48b11b965fd
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-04-08 08:55:21 +0800

    fix: refactor image processing functions to use MatLike type hints and improve image handling

--- src/image.py
+++ src/image.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
 import cv2
-import numpy as np
+from cv2.typing import MatLike
 from loguru import logger
-from numpy.typing import ArrayLike
 
 IMG_EXT = {".jpg", ".jpeg", ".png"}
 
@@ -23,11 +22,9 @@ def create_support_servant_img(
     image_np_list = _read_images(source_dir)
     final_image = _process_servant_images(image_np_list)
 
-    final_image_np = np.array(final_image)
+    cv2.imwrite(str(dest_color_file_path), final_image)
 
-    cv2.imwrite(str(dest_color_file_path), final_image_np)
-
-    final_image_np = cv2.cvtColor(final_image_np.copy(), cv2.COLOR_BGR2GRAY)
+    final_image_np = cv2.cvtColor(final_image.copy(), cv2.COLOR_BGR2GRAY)
     cv2.imwrite(str(dest_file_path), final_image_np)
     logger.info("Images processed and saved successfully.")
 
@@ -60,25 +57,23 @@ def create_support_ce_img(
 
 
 def _process_servant_images(
-    image_np_list: list[ArrayLike],
-) -> ArrayLike:
+    image_np_list: list[MatLike],
+) -> MatLike:
     """
     Process a list of images and return a combined image.
     This function resizes each image to a fixed size and crops a specific region
     from each image. The cropped images are then concatenated vertically.
 
     Args:
-        image_np_list (list[ArrayLike]): A list of numpy arrays representing the images.
+        image_np_list (list[MatLike]): A list of numpy arrays representing the images.
 
     Returns:
-        ArrayLike: A combined image as a numpy array.
+        MatLike: A combined image as a numpy array.
     """
-    new_image_np_list: list[ArrayLike] = []
+    new_image_np_list: list[MatLike] = []
     for image in image_np_list:
-        image_np = np.array(image)
-
         resize_image = cv2.resize(
-            image_np,
+            image,
             (157, 157),
             interpolation=cv2.INTER_LANCZOS4,
         )
@@ -91,13 +86,13 @@ def _process_servant_images(
         cropped_image = resize_image[y : y + height, x : x + width]
         new_image_np_list.append(cropped_image)
 
-    combined_img = np.concatenate(new_image_np_list, axis=0)
+    combined_img = cv2.vconcat(new_image_np_list)
     return combined_img
 
 
 def _read_images(
     source_dir: Path,
-) -> list[ArrayLike]:
+) -> list[MatLike]:
     """ "
     Read images from a directory and return them as a list of numpy arrays.
     This function iterates through all files in the specified directory,
@@ -109,9 +104,9 @@ def _read_images(
         source_dir (Path): The directory containing the images to be read.
 
     Returns:
-        list[ArrayLike]: A list of numpy arrays representing the images.
+        list[MatLike]: A list of numpy arrays representing the images.
     """
-    image_np_list: list[ArrayLike] = []
+    image_np_list: list[MatLike] = []
 
     images: list[Path] = []
     for ext in IMG_EXT:
